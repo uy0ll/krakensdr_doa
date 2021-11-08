8836fdb88f325102284643369ab8a706f6860690
krakenrf <78108016+krakenrf@users.noreply.github.com>
2021-11-08 16:34:04 +1300

    Update setup_init.sh

--- util/setup_init.sh
+++ util/setup_init.sh
@@ -15,4 +15,5 @@ python3 -m pip install matplotlib
 # For web interface
 python3 -m pip install dash
 python3 -m pip install dash-bootstrap-components
+python3 -m pip install dash-core-components
 python3 -m pip install gunicorn
